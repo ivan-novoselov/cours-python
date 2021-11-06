b5263a015acc76fedfeb30b490d2fd0ef2436534
ivan <i.nov123@mail.ru>
2021-11-06 23:08:41 +0300

    task1.1

---
+++ task1.3.py
@@ -0,0 +1,11 @@
+#3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
+# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+
+number = input('Введите пожалуйста число от 1 до 9: ')
+print()
+number_2 = int(number+number)
+number_3 = int(number+number+number)
+number_sum = int(number)+number_2+number_3
+print(f'{int(number)}+{number_2}+{number_3}={number_sum}')
+print()
+print(f'Результат = {number_sum}')
