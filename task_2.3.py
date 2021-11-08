7a4686f952b46d68acad4f717559cb60c147c882
ivan <i.nov123@mail.ru>
2021-11-08 22:06:50 +0300

    task_2.3

---
+++ task_2.3.py
@@ -0,0 +1,43 @@
+#3. Пользователь вводит месяц в виде целого числа от 1 до 12.
+# Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+# Напишите решения через list и через dict.
+
+
+#Решение через - dict:
+num_month_list = int(input('Номер месяца: '))
+
+my_dict = {12: 'зима', 1: 'зима', 2: 'зима', 3: 'весна', 4: 'весна', 5: 'весна', 6: 'лето', 7: 'лето', 8: 'лето', 9: 'осень', 10: 'осень', 11: 'осень'}
+print(my_dict[num_month_list])
+
+
+
+
+
+
+
+
+
+
+# my_str1 = 'hello world'
+# my_str2 = 'It is me'
+# my_str_3 = "H"+my_str1[1:]
+# print(my_str_3)
+# my_str_3 = my_str_3.split()
+# print(my_str_3)
+# print(my_str1.count('o' ))
+# print(my_str1.index('w'))
+# print(my_str1.isupper())
+# my_list = list(my_str1)
+# print(my_list)
+# my_list[0]='R'
+# my_list.insert(0,'!')
+# print(my_list)
+
+# for el in my_str1[::-1]:
+#     print(el)
+#
+# print(my_str1[::-1])
+
+
+
+#if(score_theory + score_practical > 100):
