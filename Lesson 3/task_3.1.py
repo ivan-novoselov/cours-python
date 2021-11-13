9c8e5cfa7334e2f80461eb84086b1d82e3b912b3
ivan <i.nov123@mail.ru>
2021-11-13 15:19:47 +0300

    task3.1

--- Lesson 3/task_3.1.py
+++ Lesson 3/task_3.1.py
@@ -3,15 +3,46 @@
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 
 def my_div(num1, num2):
-    result_my_div = num1 / num2
-    return result_my_div
+    if num2 == 0:
+        return 'You can not divide to 0'
+    else:
+        return num1/num2
 
-num1 = int(input('введите число:  '))
-num2 = int(input('введите число:  '))
-if num2 == 0:
-    print('число не равно "0"!!!')
-    print()
-    num2 = int(input('введите другое число:  '))
+v = int(input('num_1:  ')),int(input('num_2:  '))
+num1 = int(input('num_1:  '))
+num2 = int(input('num_2:  '))
+print(my_div(num1, num2))
 
-div_num = num1/num2
-print(f'Результат деления: {div_num}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def my_div(num1, num2):
+#     result_my_div = num1 / num2
+#     return result_my_div
+#
+# num1 = int(input('введите число:  '))
+# num2 = int(input('введите число:  '))
+# if num2 == 0:
+#     print('число не равно "0"!!!')
+#     print()
+#     num2 = int(input('введите другое число:  '))
+#
+# div_num = num1/num2
+# print(f'Результат деления: {div_num}')
